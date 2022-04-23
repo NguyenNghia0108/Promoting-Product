cb9122b149532faea6f39f0dbf20a25af959f513
NguyenNghia0108 <102567731+NguyenNghia0108@users.noreply.github.com>
2022-04-23 22:29:42 +0700

    Add file

---
+++ PromotingProducts/Report_efficiency.java
@@ -0,0 +1,13 @@
+package PromotingProducts;
+
+public class Report_efficiency {
+	public void get_cost() {
+        System.out.println("Cost: 20000$");
+	}
+	public void get_product_growth() {
+        System.out.println("Product has an outstanding growth!");
+	}
+	public void Makereport() {
+        System.out.println("Report.");
+	}
+}
