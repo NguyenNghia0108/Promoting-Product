1731791df6a6f8154c8b00efc89d2322fee61c7f
NguyenNghia0108 <102567731+NguyenNghia0108@users.noreply.github.com>
2022-04-23 22:34:45 +0700

    Add file

---
+++ AP/src/PromotingProducts/Calculate_cost.java
@@ -0,0 +1,10 @@
+package PromotingProducts;
+
+public class Calculate_cost {
+	public void get_seeding_cost() {
+        System.out.println("Seeding cost: 10000$");
+	}
+	public void get_ad_cost() {
+        System.out.println("Ads cost: 10000$");
+	}
+}
---
+++ AP/src/PromotingProducts/Prepare.java
@@ -0,0 +1,16 @@
+package PromotingProducts;
+
+public class Prepare {
+	public void Collect_data() {
+        System.out.println("Collecting data...");
+	}
+	public void Select_potential_consumer() {
+        System.out.println("Finding Potential consumer...");
+	}
+	public void Select_potential_method() {
+        System.out.println("Finding Potential method...");
+	}
+	public void Select_websites() {
+        System.out.println("Finding Websites...");
+	}
+}
